f3e1cd0dcd50d989df88701edef1de5d0ab737b4
Jan Prokop <josidek@gmail.com>
2023-06-05 22:50:47 +0200

    Updated token and information extraction
    - Separated token and information extraction.
    - Added saving of extracted tokens and information into csv files
    - Might need to add language detection in the future or add
    an program argument specifing languge of the provided material.

--- file_parser.py
+++ file_parser.py
@@ -4,11 +4,13 @@ import re
 import nltk
 import morfeusz2  # Needs a license copy within our repo i think :d
 import spacy
+import pandas as pd
+import os
 from collections import Counter
 
+    # BEFORE YOU RUN THIS CODE
     # nltk.download('wordnet') 
     # nltk.download('punkt') 
-    # pip install spacy
     # python -m spacy download pl_core_news_lg
     # python -m spacy download en_core_web_lg
 
@@ -27,43 +29,59 @@ if __name__ == "__main__":
         with open(f"./test_files/{file_name}", "rb") as f:
             text = f.read().decode('utf8')
 
-    # Clearing the text from punctuation (or rather every character that's specified in the regrex below)
+    # Clearing the text from punctuation 
+    # (or rather, every character that's specified in the regrex below - cuts out characters not used in Polish or English)
     text = re.sub('[^A-Za-z0-9ĘęÓóĄąŚśŁłŻżŹźĆćŃń ]+', ' ', text)
 
     # Important data recognition Polish
-    important_words = []
+    important_polish_phrases = []
     nlpPL = spacy.load("pl_core_news_lg")
     important_text_polish = nlpPL(text)
     for ent in important_text_polish.ents:
         # print(ent.text, ent.label_)
-        important_words.append(ent.text)
+        important_polish_phrases.append(ent.text)
+    
     # Important data recognition Engish
+    important_english_phrases = []
     nlpEN = spacy.load("en_core_web_lg")
     important_text_english = nlpEN(text)
     for ent in important_text_english.ents:
-        if ent.label_ == "DATE":
-            # print(ent.text, ent.label_)
-            important_words.append(ent.text)
+        # print(ent.text, ent.label_)
+        important_english_phrases.append(ent.text)
 
-    # Creating a dictionary of lemmatized words with their counted appearances
-    words = important_words
-    lem_words = []
-    # FOR POLISH
+    # Creating a list of lemmatized words with their counted appearances
+    # At the moment, two models are used to lemmatize gathered words consecutively, first for polish, second for english lemmatization
+    words = text.split()
+    lemmatized_words = []
+    # POLISH
     morf = morfeusz2.Morfeusz() 
     last_i = -1
     for i, j, interp in morf.analyse(' '.join(words)):
+        # using last_i number to only save the first interpretation of a word
         if last_i < i:
-            lem_words.append(interp[1].split(":")[0])
+            lemmatized_words.append(interp[1].split(":")[0])
             last_i = i
-    # FOR ENGLISH
+    # ENGLISH
     lemmatizer = nltk.WordNetLemmatizer()
-    lem_words = [lemmatizer.lemmatize(word) for word in lem_words]
-    # COUNT
-    word_count_dict = dict(Counter(lem_words).items())
-
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in lemmatized_words]
+    # Counting
+    word_count_dict = dict(Counter(lemmatized_words).items())
     # Creating a list of words sorted by the occurance number
     sorted_word_count = [(k, word_count_dict[k]) for k in sorted(word_count_dict, key=word_count_dict.get, reverse=True)]
-
-    for element in sorted_word_count:
-        print(element)    
     
+    # Creating output folder
+    if not os.path.exists("./output"):
+        os.makedirs("./output")
+
+    # Saving the tokens and their count
+    data = {'word': [word for (_, word) in sorted_word_count], 'count': [count for (count, _) in sorted_word_count]}
+    df = pd.DataFrame(data)
+    df.to_csv("./output/WordCount.csv")
+
+    # Saving extracted important phrases
+    data = {'phrases': important_polish_phrases}
+    df = pd.DataFrame(data)
+    df.to_csv("./output/PolishPhrases.csv")
+    data = {'phrases': important_english_phrases}
+    df = pd.DataFrame(data)
+    df.to_csv("./output/EnglishPhrases.csv")
