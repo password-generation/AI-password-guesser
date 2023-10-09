1afdb6486ffed9dda08552c24ad65f869d57c63f
Dominika Bochenczyk <dominika.bochenczyk@gmail.com>
2023-10-09 19:37:41 +0200

    Add schedule parsing

--- password_mangler/yaml_parser.py
+++ password_mangler/yaml_parser.py
@@ -1,4 +1,5 @@
 import yaml
+from rules_applier import ManglingEpochType
 import password_rules
 
 
@@ -6,11 +7,33 @@ def parse_yaml(filename):
     with open(filename, "r") as file:
         rules = yaml.safe_load(file)
 
-    unary_names, binary_names = rules["unary_rules"], rules["binary_rules"]
+    unary_names, binary_names, mangling_schedule = (
+        rules["unary_rules"],
+        rules["binary_rules"],
+        rules["mangling_schedule"],
+    )
 
-    unary, binary = get_funcs(unary_names), get_funcs(binary_names)
+    unary, binary = (
+        get_funcs(unary_names),
+        get_funcs(binary_names),
+    )
 
-    return unary, binary
+    schedule = get_schedule(mangling_schedule)
+
+    return unary, binary, schedule
+
+
+def get_schedule(names):
+    schedule = []
+    for name in names:
+        if name == "unary":
+            schedule.append(ManglingEpochType.UNARY)
+        elif name == "binary":
+            schedule.append(ManglingEpochType.BINARY)
+
+        # TODO handling other cases
+
+    return schedule
 
 
 def get_funcs(names, module=password_rules):
@@ -22,8 +45,8 @@ def get_funcs(names, module=password_rules):
 
 
 if __name__ == "__main__":
-    unary, binary = parse_yaml("config.yaml")
-    print(unary, binary)
+    unary, binary, schedule = parse_yaml("config.yaml")
+    print(unary, binary, schedule)
 
     print("--- unary ---")
     s1 = "p@ssW0rD"
