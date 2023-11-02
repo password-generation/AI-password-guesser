da75935f4e78ded18e6d00f586fbf5c9277f8002
Dominika Bochenczyk <dominika.bochenczyk@gmail.com>
2023-11-02 23:50:56 +0100

    date parsing with binary mask and upgraded

--- password_mangler/dates_parser.py
+++ password_mangler/dates_parser.py
@@ -3,37 +3,29 @@ from dateutil.parser import parse
 
 
 def extract_parse_dates(tokens: list[Token]):
-    other_tokens = set(filter(lambda x: LabelType.DATE not in x.labels, tokens))
-    date_tokens = list(filter(lambda x: LabelType.DATE in x.labels, tokens))
+    date_mask = LabelType.to_binary_mask([LabelType.DATE])
+    other_tokens = set(filter(lambda x: date_mask != x.binary_mask, tokens))
+    date_tokens = list(filter(lambda x: date_mask == x.binary_mask, tokens))
+
+    # TODO
+    # language version here
 
     for token in date_tokens:
         try:
             datetime = parse(token.text)
-            labels = token.labels
-
-            day = datetime.date().day
-            month = datetime.date().month
-            year = datetime.date().year
-
-            short_year = None
-            if year < 2000 and year > 1900:
-                short_year = year % 100
-
-            day_month = day * 100 + month
-
-            new_tokens = [
-                Token(str(day), labels),
-                Token(str(month), labels),
-                Token(str(year), labels),
-                Token(str(day_month), labels),
-            ]
-
-            if short_year:
-                new_tokens.append(Token(str(short_year), labels))
-
-            other_tokens.update(new_tokens)
-
         except:  # not a valid datetime format to parse, we skip it
             continue
+        else:
+            labels = token.binary_mask
+            d, m, y = datetime.date().day, datetime.date().month, datetime.date().year
+
+            day = str(d) if len(str(d)) > 1 else "0" + str(d)
+            month = str(m) if len(str(m)) > 1 else "0" + str(m)
+            year = str(y)
+            short_year = year[2:]
+            day_month = day + month
+
+            time_vars = [day, month, year, short_year, day_month]
+            other_tokens.update([Token(t, labels) for t in time_vars])
 
     return list(other_tokens)
